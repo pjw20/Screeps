c45f4ca57a2d8cfd23e5cd4cdae72f7dbeaa3a56
Paul White <thewhitey@gmail.com>
2016-12-03 19:32:42 +0000

    no harvesting from upgraders

--- upgrade.js
+++ upgrade.js
@@ -55,14 +55,6 @@ module.exports.run = function(creep, needCreeps)
                 return;
             }
         }
-        else
-        {
-            let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE)
-            {
-                creep.moveTo(source);
-            }
-        }
     }
     else
     {
