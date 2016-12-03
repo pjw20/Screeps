a52e0516efdda0095cc3bbbe78983ff2aaab9e4a
Paul White <thewhitey@gmail.com>
2016-12-03 14:09:04 +0000

    harvester improves

--- harvest.js
+++ harvest.js
@@ -12,20 +12,23 @@ module.exports.run = function(creep)
     else
     {
         //we are full
+        let target = 0;
         if (creep.room.memory.numHaulers != 0)
         {
-            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (o) => (o.structureType == STRUCTURE_CONTAINER) && (o.store[RESOURCE_ENERGY] < o.storeCapacity) && o.isActive() == true});
-
+            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (o) => (o.structureType == STRUCTURE_CONTAINER) && (o.store[RESOURCE_ENERGY] < o.storeCapacity) && o.isActive() == true});
+            if (creep.pos.getRangeTo(target) > 15)
+            {
+                //too far away
+                target = 0;
+            }
         }
 
         if (!target)
         {
-            //console.log(target);
             target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (o) => (o.structureType == STRUCTURE_EXTENSION) && (o.energy < o.energyCapacity) ||
-            (o.structureType == STRUCTURE_SPAWN) && (o.energy < o.energyCapacity)});
+                                                                                    (o.structureType == STRUCTURE_SPAWN) && (o.energy < o.energyCapacity)});
         }
 
-        //console.log(target);
         //TODO: what to do if all structures are full
         if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
         {
