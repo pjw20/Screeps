dd1747f10fe9edf5b124cbddb5ca2b15aeb4a5ec
Paul White <thewhitey@gmail.com>
2016-12-03 12:32:15 +0000

    screep say

--- creep.js
+++ creep.js
@@ -2,10 +2,44 @@ module.exports.run = function(creep)
 {
     if (creep.carry.energy == creep.carryCapacity)
     {
+        switch (creep.memory.role)
+        {
+            case "harvester":
+                creep.say("dr");
+                break;
+            case "builder":
+                creep.say("bu");
+                break;
+            case "upgrader":
+                creep.say("up");
+                break;
+            case "hauler":
+                creep.say("ha");
+                break;
+            default:
+                break;
+        }
         creep.memory.working = true;
     }
     else if (creep.carry.energy == 0)
     {
+        switch (creep.memory.role)
+        {
+            case "harvester":
+                creep.say("hv");
+                break;
+            case "builder":
+                creep.say("co");
+                break;
+            case "upgrader":
+                creep.say("co");
+                break;
+            case "hauler":
+                creep.say("co");
+                break;
+            default:
+                break;
+        }
         creep.memory.working = false;
     }
 };
