c1187e1c9770027aaf4db162088e8dc6152eecd8
Paul White <thewhitey@gmail.com>
2016-12-05 21:30:25 +0000

    .

--- upgrade.js
+++ upgrade.js
@@ -2,15 +2,31 @@ module.exports.run = function(creep, needCreeps)
 {
     if (!creep.memory.working)
     {
-        //we are not full
-        if (needCreeps == false)
+        if (creep.memory.target)
         {
-            if (creep.memory.target)
+            let result = creep.withdraw(Game.getObjectById(creep.memory.target), RESOURCE_ENERGY);
+            if (result == ERR_NOT_IN_RANGE)
             {
-                let result = creep.withdraw(Game.getObjectById(creep.memory.target), RESOURCE_ENERGY);
+                creep.moveTo(Game.getObjectById(creep.memory.target));
+            }
+            else if (result == OK || result == ERR_INVALID_TARGET || result == ERR_NOT_ENOUGH_RESOURCES)
+            {
+                creep.memory.target = 0;
+            }
+            return;
+        }
+
+        let containers = creep.room.find(FIND_STRUCTURES, {filter: (o) => (o.structureType == STRUCTURE_CONTAINER) && (o.store[RESOURCE_ENERGY] > 0) && (o.isActive() == true)});
+
+        for (let container of containers)
+        {
+            if (creep.pos.getRangeTo(container) < 8)
+            {
+                creep.memory.target = container.id;
+                let result = creep.withdraw(container, RESOURCE_ENERGY);
                 if (result == ERR_NOT_IN_RANGE)
                 {
-                    creep.moveTo(Game.getObjectById(creep.memory.target));
+                    creep.moveTo(container);
                 }
                 else if (result == OK || result == ERR_INVALID_TARGET || result == ERR_NOT_ENOUGH_RESOURCES)
                 {
@@ -18,26 +34,11 @@ module.exports.run = function(creep, needCreeps)
                 }
                 return;
             }
-            let containers = creep.room.find(FIND_STRUCTURES, {filter: (o) => (o.structureType == STRUCTURE_CONTAINER) && (o.store[RESOURCE_ENERGY] > 0) && (o.isActive() == true)});
+        }
 
-            for (let container of containers)
-            {
-                if (creep.pos.getRangeTo(container) < 8)
-                {
-                    creep.memory.target = container.id;
-                    let result = creep.withdraw(container, RESOURCE_ENERGY);
-                    if (result == ERR_NOT_IN_RANGE)
-                    {
-                        creep.moveTo(container);
-                    }
-                    else if (result == OK || result == ERR_INVALID_TARGET || result == ERR_NOT_ENOUGH_RESOURCES)
-                    {
-                        creep.memory.target = 0;
-                    }
-                    return;
-                }
-            }
-
+        //we are not full
+        if (needCreeps == false)
+        {
             let target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (o) => ((o.structureType == STRUCTURE_CONTAINER) && (o.store[RESOURCE_ENERGY] > 0) && (o.isActive() == true)) ||
                                                                                     (o.structureType == STRUCTURE_EXTENSION || o.structureType == STRUCTURE_SPAWN) && (o.energy > 1)});
             if (target)
