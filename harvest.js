c570c0d7f88f3625f90ca2dc1313107fa7997c46
Paul White <thewhitey@gmail.com>
2016-12-10 21:30:53 +0000

    .

--- harvest.js
+++ harvest.js
@@ -7,7 +7,7 @@ module.exports.run = function(creep)
             creep.memory.targetRoom = creep.room.name;
         }
 
-        if (creep.room == creep.memory.targetRoom)
+        if (creep.room.name == creep.memory.targetRoom)
         {
             //we are not full
             let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
@@ -19,38 +19,46 @@ module.exports.run = function(creep)
     }
     else
     {
-        //we are full
-        let target = 0;
-        if (creep.room.memory.numHaulers != 0)
+        if (creep.memory.homeRoom == undefined)
         {
-            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (o) => (o.structureType == STRUCTURE_CONTAINER) && (o.store[RESOURCE_ENERGY] < o.storeCapacity) && o.isActive() == true});
-            if (creep.pos.getRangeTo(target) > 15)
-            {
-                //too far away
-                target = 0;
-            }
+            creep.memory.homeRoom = creep.room.name;
         }
 
-        if (!target)
+        if (creep.room.name == creep.memory.homeRoom)
         {
-            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (o) => (o.structureType == STRUCTURE_EXTENSION) && (o.energy < o.energyCapacity) ||
-                                                                                    (o.structureType == STRUCTURE_SPAWN) && (o.energy < o.energyCapacity)});
-        }
-
-        if (target)
-        {
-            if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+            //we are full
+            let target = 0;
+            if (creep.room.memory.numHaulers != 0)
             {
-                creep.moveTo(target);
-            }
-        }
-        else
-        {
-            if(creep.room.controller)
-            {
-                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE)
+                target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (o) => (o.structureType == STRUCTURE_CONTAINER) && (o.store[RESOURCE_ENERGY] < o.storeCapacity) && o.isActive() == true});
+                if (creep.pos.getRangeTo(target) > 15)
                 {
-                    creep.moveTo(creep.room.controller);
+                    //too far away
+                    target = 0;
+                }
+            }
+
+            if (!target)
+            {
+                target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (o) => (o.structureType == STRUCTURE_EXTENSION) && (o.energy < o.energyCapacity) ||
+                (o.structureType == STRUCTURE_SPAWN) && (o.energy < o.energyCapacity)});
+            }
+
+            if (target)
+            {
+                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                {
+                    creep.moveTo(target);
+                }
+            }
+            else
+            {
+                if(creep.room.controller)
+                {
+                    if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE)
+                    {
+                        creep.moveTo(creep.room.controller);
+                    }
                 }
             }
         }
